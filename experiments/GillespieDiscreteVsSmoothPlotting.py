42d1c45842cb6726041bbe5ddce7f80c1455d642
Vlad <vlad.sergeev@gmail.com>
2016-08-30 17:30:28 +0200

    fix the indexing bug

--- experiments/GillespieDiscreteVsSmoothPlotting.py
+++ experiments/GillespieDiscreteVsSmoothPlotting.py
@@ -4,6 +4,7 @@ import autograd.numpy as np
 import pylab as plt
 
 def gillespieDiscreteVsSmoothPlot():
+
     setup = Setup(yaml_file_name="lotka_volterra.yaml")
     propensities = setup.get_propensity_list()
     parameters = setup.get_parameter_list()
@@ -14,11 +15,13 @@ def gillespieDiscreteVsSmoothPlot():
     seed = 1000
 
     my_gillespieUp = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing = False, seed = seed)
-    aMean = my_gillespieUp.run_simulation(*parameters)
+    mean = my_gillespieUp.run_simulation(*parameters)
     my_gillespieSmooth = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing = True, seed = seed)
-    aMeanS = my_gillespieSmooth.run_simulation(*parameters)
+    meanS = my_gillespieSmooth.run_simulation(*parameters)
 
     tau = np.linspace(0,T,51)
+    aMean = mean[:50]
+    aMeanS = meanS[:50]
 
     plt.plot(tau[1:],aMeanS,label="Smooth A")
     plt.step(tau[1:],aMean,label='Step A')
