Fixing a bug with gradients.  Arbitrary count of species is allowed.  Unit-tests pass.

experiments/GillespieAdam.py
@@ -61,7 +61,7 @@ def gillespieGradientWalk():
     my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,
                              increments=incr,nPaths = nPaths,T=T,useSmoothing=True, seed = seed, numProc = numProc)
 
-    observed_data = my_gillespie.run_simulation(np.log(parameters))
+    observed_data = my_gillespie.run_simulation(parameters)
 
     starting_parameters = [x for x in parameters]
     idx = 0
@@ -84,7 +84,7 @@ def gillespieGradientWalk():
 
     lossFunctionGrad = value_and_grad(lossFunction,idx)
 
-    cost_list,param0,param1,param2 = adam(lossFunctionGrad,np.log(starting_parameters), num_iters=50)
+    cost_list,param0,param1,param2 = adam(lossFunctionGrad,starting_parameters, num_iters=50)
     fig,(axC, ax0, ax1, ax2) = plt.subplots(nrows=4,sharex=True)
     x = [x for x in range(len(param1))]
 

experiments/GillespieBumpAndRevalVsAutogradPlotting.py
@@ -19,17 +19,17 @@ def gillespieBumpAndRevalVsAutograd():
     idx = 0
 
     my_gillespie = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing=True, seed = seed)
-    gradients = my_gillespie.take_gradients(np.log(parameters))
+    gradients = my_gillespie.take_gradients(parameters)
     aGradient = gradients[:50]
     bGradient = gradients[50:]
 
     parameters1 = [x for x in parameters]
     parameters1[idx] = parameters[idx]+delta
     my_gillespieUp = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing=True, seed = seed)
-    bumpUp = my_gillespieUp.run_simulation(np.log(parameters1))
+    bumpUp = my_gillespieUp.run_simulation(parameters1)
     parameters1[idx] = parameters[idx]-delta
     my_gillespieDown = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing=True, seed = seed)
-    bumpDown = my_gillespieDown.run_simulation(np.log(parameters1))
+    bumpDown = my_gillespieDown.run_simulation(parameters1)
     aBumpUp = bumpUp[:50]
     aBumpDown = bumpDown[:50]
     bBumpUp = bumpUp[50:]

experiments/GillespieGradientsTest.py
@@ -18,9 +18,9 @@ def gillespieGradientWalk():
 
     idx = 1
 
-    my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,
+    my_gillespie = Gillespie(species=species,propensities=propensities,
                              increments=incr,nPaths = nPaths,T=T,useSmoothing=True, seed = seed, numProc = numProc)
-    observed_data = my_gillespie.run_simulation(np.log(parameters))
+    observed_data = my_gillespie.run_simulation(parameters)
 
     starting_parameters = [x for x in parameters]
     starting_parameters[idx] = parameters[idx]+parameters[idx]*0.05
@@ -31,7 +31,7 @@ def gillespieGradientWalk():
 
     def lossFunction(parameters):
 
-        gillespieGrad = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr,
+        gillespieGrad = Gillespie(species=species,propensities=propensities,increments=incr,
                                   nPaths = nPaths,T=T,useSmoothing=True, seed = seed, numProc = numProc )
 
         simulated_data = gillespieGrad.run_simulation(parameters)
@@ -44,7 +44,7 @@ def gillespieGradientWalk():
         print "Iteration {}. Current value of a parameter at index {} is {}".format(i, idx,starting_parameters[idx])
 
         lossFunctionGrad = value_and_grad(lossFunction)
-        value, gradient = lossFunctionGrad(np.log(starting_parameters))
+        value, gradient = lossFunctionGrad(starting_parameters)
         starting_parameters[idx] = starting_parameters[idx]-gradient[idx]*dw[idx]
         print "\n Loss Function Value: \n {0} \n Gradient: \n {1} ".format(value,gradient)
         i+=1

experiments/GillespiePaperLv.py
@@ -15,9 +15,9 @@ def gillespieDiscreteVsSmoothPlot():
     seed = 1000
 
     my_gillespieUp = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing = False, seed = seed)
-    mean = my_gillespieUp.run_simulation(np.log(parameters))
+    mean = my_gillespieUp.run_simulation(parameters)
     my_gillespieSmooth = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing = True, seed = seed)
-    meanS = my_gillespieSmooth.run_simulation(np.log(parameters))
+    meanS = my_gillespieSmooth.run_simulation(parameters)
 
     tau = np.linspace(0,T,51)
     aMean = mean[:50]

experiments/GillespieParameterDelta.py
@@ -18,8 +18,8 @@ def gillespieParameterVariance():
     tau = np.linspace(0,T,51)
     idx = 0
 
-    my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False, seed = seed)
-    discrete_sim_data = my_gillespie.run_simulation(np.log(parameters))
+    my_gillespie = Gillespie(species=species,propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False, seed = seed)
+    discrete_sim_data = my_gillespie.run_simulation(parameters)
     discrete_a = discrete_sim_data[:50]
     discrete_b = discrete_sim_data[50:]
 
@@ -31,8 +31,8 @@ def gillespieParameterVariance():
         starting_parameters = [x for x in parameters]
         starting_parameters[idx] = parameters[idx]+step*i+offset
         starting_parameters = np.array(starting_parameters)
-        my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr,nPaths = nPaths,T=T, useSmoothing=True, seed = seed2 + i*10000)
-        res = my_gillespie.run_simulation(np.log(starting_parameters))
+        my_gillespie = Gillespie(species=species,propensities=propensities,increments=incr,nPaths = nPaths,T=T, useSmoothing=True, seed = seed2 + i*10000)
+        res = my_gillespie.run_simulation(starting_parameters)
         a_data.append(res[:50])
         b_data.append(res[50:])
         print(starting_parameters[idx])

experiments/GillespieVarianceRandomNumbers.py
@@ -20,13 +20,13 @@ def gillespieRandomnessVariance():
     timeGrid = np.linspace(0, T, numSteps)[1:]
     a_diff = []
     b_diff = []
-    my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False,numSteps = numSteps,numProc = 2, seed = seed)
-    discrete_sim_data = my_gillespie.run_simulation(np.log(parameters))
+    my_gillespie = Gillespie(species=species,propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False,numSteps = numSteps,numProc = 2, seed = seed)
+    discrete_sim_data = my_gillespie.run_simulation(parameters)
     base_a = discrete_sim_data[:numSteps-1]
     base_b = discrete_sim_data[numSteps-1:]
     for i in range(5):
-        my_gillespie = Gillespie(a=species[0],b=species[1],propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False,numSteps = numSteps, numProc = 2, seed = seed2+i*stride)
-        discrete_sim_data = my_gillespie.run_simulation(np.log(parameters))
+        my_gillespie = Gillespie(species=species,propensities=propensities,increments=incr,nPaths = nPaths,T=T,useSmoothing=False,numSteps = numSteps, numProc = 2, seed = seed2+i*stride)
+        discrete_sim_data = my_gillespie.run_simulation(parameters)
         discrete_a = discrete_sim_data[:numSteps-1]
         discrete_b = discrete_sim_data[numSteps-1:]
         a_diff.append([a - b for a, b in zip(discrete_a, base_a)])

gillespie/Gillespie.py
@@ -80,9 +80,10 @@ class Gillespie(object):
 
     def run_path(self, parameters, seed_offset):
 
+
         self.timeGrid = np.linspace(0,self.T,self.numSteps)
-        self.parameters = parameters
-        self.alpha0 = lambda x: sum(pair[1](pair[0],x) for pair in zip(parameters,self.propensities))
+        self.parameters = np.log(parameters)
+        self.alpha0 = lambda x: sum(pair[1](pair[0],x) for pair in zip(self.parameters,self.propensities))
 
         tauSamples = []
         tauSamples.append(0.0)

tests/GillespieTestRegression.py
@@ -2,6 +2,7 @@ import unittest
 from gillespie import Gillespie
 from gillespie import Setup
 import autograd.numpy as np
+from numpy import testing
 
 class GillespieTestSuiteRegression(unittest.TestCase):
 
@@ -18,7 +19,7 @@ class GillespieTestSuiteRegression(unittest.TestCase):
                                    T=T, useSmoothing=False, numProc = 2, seed = seed)
 
         parameters = np.array(parameters)
-        result = my_gillespieUp.run_simulation(np.log(parameters))
+        result = my_gillespieUp.run_simulation(parameters)
         expected = [1286.2, 1300.4000000000001, 920.70000000000005, 518.79999999999995, 283.19999999999999,
                    179.09999999999999, 133.69999999999999, 126.3, 132.09999999999999, 151.59999999999999,
                    192.30000000000001, 250.30000000000001, 349.10000000000002, 477.69999999999999, 665.70000000000005,
@@ -59,7 +60,7 @@ class GillespieTestSuiteRegression(unittest.TestCase):
         parameters = np.array(parameters)
 
         gillespieGrad = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing=True,numProc=2, seed=seed)
-        gradient = gillespieGrad.take_gradients(np.log(parameters))
+        gradient = gillespieGrad.take_gradients(parameters)
 
         expected = [0.20615962712971669, 0.20765962497976251, 0.20946011564211109, 0.21122237250866763,
                    0.21271498482749313, 0.21392790067199691, 0.21528167987721566, 0.21603052898934599,
@@ -93,8 +94,8 @@ class GillespieTestSuiteRegression(unittest.TestCase):
         aGradient = gradient[:50]
         bGradient = gradient[50:]
 
-        self.assertListEqual(list(zip(*aGradient)[0]), expectedGradA)
-        self.assertListEqual(list(zip(*bGradient)[0]), expectedGradB)
+        testing.assert_array_almost_equal(np.array(zip(*aGradient)[0]), np.array(expectedGradA))
+        testing.assert_array_almost_equal(np.array(zip(*bGradient)[0]), np.array(expectedGradB))
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()