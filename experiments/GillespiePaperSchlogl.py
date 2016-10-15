9169393fb2e1335099a6e5dd9ad8926136b99ff3
Vlad <vlad.sergeev@gmail.com>
2016-10-16 00:24:55 +0200

    Correcting the model definition file to match the one in the paper.  showing separate plots for species

--- experiments/GillespiePaperSchlogl.py
+++ experiments/GillespiePaperSchlogl.py
@@ -10,8 +10,8 @@ def gillespieDiscreteVsSmoothPlot():
     parameters = np.array(setup.get_parameter_list())
     species = setup.get_species()
     incr = setup.get_increments()
-    nPaths = 2
-    T = 15
+    nPaths = 10
+    T = 4
     seed = 1000
 
     my_gillespieUp = Gillespie(species=species,propensities=propensities,increments=incr, nPaths = nPaths,T=T,useSmoothing = False, seed = seed)
@@ -22,13 +22,18 @@ def gillespieDiscreteVsSmoothPlot():
     tau = np.linspace(0,T,51)
     aMean = mean[:50]
     aMeanS = meanS[:50]
-    bMean = mean[50:]
-    bMeanS = meanS[50:]
+    bMean = mean[50:100]
+    bMeanS = meanS[50:100]
+    cMean = mean[100:]
+    cMeanS = meanS[100:]
 
-    plt.plot(tau[1:],aMeanS,label="Smooth A")
-    plt.step(tau[1:],aMean,label='Step A')
-    plt.plot(tau[1:],bMeanS,label="Smooth B")
-    plt.step(tau[1:],bMean,label='Step B')
+    fig,(ax0, ax1, ax2) = plt.subplots(nrows=3,sharex=True)
+    ax0.plot(tau[1:],aMeanS,label="Smooth A")
+    ax0.step(tau[1:],aMean,label='Step A')
+    ax1.plot(tau[1:],bMeanS,label="Smooth B")
+    ax1.step(tau[1:],bMean,label='Step B')
+    ax2.plot(tau[1:],cMeanS,label="Smooth C")
+    ax2.step(tau[1:],cMean,label='Step C')
     legend = plt.legend(loc='upper center', shadow=True)
     frame = legend.get_frame()
     frame.set_facecolor('0.90')
