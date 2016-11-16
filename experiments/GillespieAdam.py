af5461bf7c9e18b964e9f275f513d342bf8d1c1c
Vlad <vlad.sergeev@gmail.com>
2016-11-16 23:34:10 +0100

    changes inspired by Ted's code, slightly modified loss function (divide by 100) and logs everywhere

--- experiments/GillespieAdam.py
+++ experiments/GillespieAdam.py
@@ -20,58 +20,61 @@ def gillespieGradientWalk():
     species = setup.get_species()
     incr = setup.get_increments()
     nPaths = 1 #setup.get_number_of_paths()
-    T = 2.0 #setup.get_time_horizon()
+    T = 1.0 #setup.get_time_horizon()
     seed = 100
     numProc = 1
+    idx = 0
 
     my_gillespie = Gillespie(species=species,propensities=propensities,
                              increments=incr,nPaths = nPaths,T=T,useSmoothing=True, seed = seed, numProc = numProc)
 
     observed_data = my_gillespie.run_simulation(parameters)
-    starting_parameters = [x for x in parameters]
-    idx = 0
-    starting_parameters[0] = parameters[0]+parameters[0]*0.2
-    #starting_parameters[1] = parameters[1]+parameters[1]*0.2
-    #starting_parameters[2] = parameters[2]+parameters[2]*0.2
 
-    print "{} \n".format(np.array(observed_data[:10]))
-    parameters = np.array(parameters)
-    starting_parameters = np.array(starting_parameters)
+    true_log_parameters = np.log(parameters)
+    log_parameters = np.log( parameters )
 
-    def lossFunction(parameters,one_more):
+    log_parameters[0] += 0.5*np.random.randn(1)
+    log_parameters[1] += 0.5*np.random.randn(1)
+    log_parameters[2] += 0.5*np.random.randn(1)
+
+    print "True Log-Params: {}".format(true_log_parameters)
+    print "Where we start: {} \n".format(log_parameters)
+
+    def lossFunction(log_parameters,dummy):
 
         gillespieGrad = Gillespie(species=species,propensities=propensities,increments=incr,
                                   nPaths = nPaths,T=T,useSmoothing=True, seed = seed, numProc = numProc )
 
-        simulated_data = gillespieGrad.run_simulation(parameters)
+        simulated_data = gillespieGrad.run_simulation(np.exp(log_parameters))
 
-        return sum(0.5*(np.array(simulated_data)-np.array(observed_data))**2)
+        #return sum(0.5*(np.array(simulated_data)-np.array(observed_data))**2)
+        return np.sum(np.square( (np.array(simulated_data)-np.array(observed_data))/100.0))
 
     lossFunctionGrad = value_and_grad(lossFunction,idx)
 
-    cost_list,param0,param1,param2 = adam(lossFunctionGrad,starting_parameters, num_iters=200)
+    cost_list,param0,param1,param2 = adam(lossFunctionGrad,np.array(log_parameters).copy(), num_iters=40)
 
     fig,(axC, ax0, ax1, ax2) = plt.subplots(nrows=4,sharex=True)
     x = [x for x in range(len(param1))]
 
-    p0 = [parameters[0] for _ in range(len(x))]
-    p1 = [parameters[1] for _ in range(len(x))]
-    p2 = [parameters[2] for _ in range(len(x))]
+    p0 = [true_log_parameters[0] for _ in range(len(x))]
+    p1 = [true_log_parameters[1] for _ in range(len(x))]
+    p2 = [true_log_parameters[2] for _ in range(len(x))]
 
     axC.plot(x,cost_list,label="Cost",linewidth=2)
     axC.set_title("Loss")
 
     ax0.plot(x,param0,label="Parameter 0",linewidth=2)
     ax0.plot(x,p0,label="Actual Value ",linewidth=4)
-    ax0.set_title("c1:  True = {}, Start = {}, Result = {}".format(parameters[0],starting_parameters[0],param0[-1]))
+    ax0.set_title("c1:  True = {}, Start = {}, Result = {}".format(true_log_parameters[0],log_parameters[0],param0[-1]))
 
     ax1.plot(x,param1,label="Parameter 1",linewidth=2)
     ax1.plot(x,p1,label="Actual Value ",linewidth=4)
-    ax1.set_title("c2: True = {}, Start = {}, Result = {}".format(parameters[1],starting_parameters[1],param1[-1]))
+    ax1.set_title("c2: True = {}, Start = {}, Result = {}".format(true_log_parameters[1],log_parameters[1],param1[-1]))
 
     ax2.plot(x,param2,label="Parameter 2",linewidth=2)
     ax2.plot(x,p2,label="Actual Value ",linewidth=4)
-    ax2.set_title("c3: True = {}, Start = {}, Result = {}".format(parameters[2],starting_parameters[2],param2[-1]))
+    ax2.set_title("c3: True = {}, Start = {}, Result = {}".format(true_log_parameters[2],log_parameters[2],param2[-1]))
 
     plt.savefig("convergence.png")
     plt.show()
--- gillespie/GillespieAdam.py
+++ gillespie/GillespieAdam.py
@@ -5,29 +5,26 @@ import math
 """Adam as described in http://arxiv.org/pdf/1412.6980.pdf.
 It's basically RMSprop with momentum and some correction terms."""
 
-def adam(grad, init_params, callback=None, num_iters=200,
-         step_size=np.array([0.5,0.5,0.5]), b1=0.9, b2=0.999, eps=10 ** -8):
+def adam(grad, log_params, callback=None, num_iters=200,
+         step_size=np.array([0.05,0.05,0.05]), b1=0.9, b2=0.999, eps=10 ** -8):
 
-    flattened_grad, unflatten, x = flatten_func(grad, init_params)
+    flattened_grad, unflatten, x = flatten_func(grad, log_params)
 
     cost_list = []
     cost_list.append(1e10)
     param1 = []
     param2 = []
     param3 = []
-    param1.append(init_params[0])
-    param2.append(init_params[1])
-    param3.append(init_params[2])
+    param1.append(log_params[0])
+    param2.append(log_params[1])
+    param3.append(log_params[2])
     m = np.zeros(len(x))
     v = np.zeros(len(x))
     update = 0
     #step_size[1:] = 0.0
     for i in range(1,num_iters):
 
-        #if i % 10 == 0:
-        #    step_size*=0.5
-
-        if cost_list[-1] < 5000:
+        if cost_list[-1] < 5.0:
             break
 
         g = flattened_grad(x, i)
@@ -39,7 +36,7 @@ def adam(grad, init_params, callback=None, num_iters=200,
         print "iteration {} cost {} parameters {} log update {} ".format(i, cost, unflatten(x), update)
         update = step_size*gamma*m/np.sqrt(i*v)
         if callback: callback(unflatten(x), i, unflatten(g))
-        x = np.exp(np.log(x) - update)
+        x = x - update
         unflattened_x = unflatten(x)
         cost_list.append(cost)
         param1.append(unflattened_x[0])
